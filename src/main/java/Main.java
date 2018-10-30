28ad82f78853ab118c857b86f26fe745cc00353e
Mihail Saytiev <saitiev2008@gmail.com>
2018-10-30 23:08:26 +0600

    fix codestyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,21 +10,21 @@ import java.io.IOException;
  * main class
  */
 public final class Main {
-    private Main(){}
+    private Main() {
+    }
 
     /**
-     *
      * @param args from command line
-     *@throws ReaderException This class signals that read error occurred
-     *@throws WriterException This class signals that read error occurred
-     *@throws IOException Signals that an I/O exception of some sort has occurred. This class is the general class of
-     *          exceptions produced by failed or interrupted I/O operations.
+     * @throws ReaderException This class signals that read error occurred
+     * @throws WriterException This class signals that read error occurred
+     * @throws IOException     Signals that an I/O exception of some sort has occurred. This class is the general class of
+     *                         exceptions produced by failed or interrupted I/O operations.
      */
     public static void main(final String[] args) throws ReaderException, WriterException, IOException {
         Formatter formatter = new Formatter();
         StringWriter stringWriter = new StringWriter();
         StringReader stringReader = new StringReader("{{{{}}}}");
-       formatter.format(stringReader, stringWriter);
+        formatter.format(stringReader, stringWriter);
         System.out.print(stringWriter.getOutput());
     }
 }
