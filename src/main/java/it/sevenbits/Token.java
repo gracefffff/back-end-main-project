811f0a5d0a171686351431a9ee1ea2e7bd6cd543
Mihail Saytiev <saitiev2008@gmail.com>
2018-11-20 23:32:32 +0600

    have made more javadocs

--- src/main/java/it/sevenbits/Token.java
+++ src/main/java/it/sevenbits/Token.java
@@ -1,10 +1,18 @@
 package it.sevenbits;
 
+/**
+ * this is  Token which Lexer created
+ */
 public class Token implements IToken {
     private String name;
     private String lexeme;
 
-    public Token(final String name, final String lexeme) {
+    /**
+     *
+     * @param name this is name of Token
+     * @param lexeme this is characters(lexeme) that this token contains
+     */
+     Token(final String name, final String lexeme) {
         this.name = name;
         this.lexeme = lexeme;
     }
